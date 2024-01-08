ab6867c255802079979c03ff517f901ab0e6e318
walid hasan <walidhasanbc@gmail.com>
2024-01-08 19:04:00 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+document.write ("WALID HASAN <br>");
+document.write ("walid walid <br>");
+
+// numer 2// 
+
+
+var name = "walid "
+var age = "21"
+
+document.write(name);
+document.write(age);
+//numer 3//
+
+var name ;
+var age ;
+
+name ="Emam";
+age = "22";
+
+document.write(name);
+document.write(age);
+
+// numer mathod//
+
+
+var number = 2.253;
+
+console.log(number.toFixed(1))
+
+console.log(number.toPrecision(2))
+
+
+// string concatenate//
+
+document.write("hasan" +"Emam");
+
+var firstname = "walid";
+var lasatname = "Emam";
+
+fullname = firstname + lasatname ;
+document.write(fullname);
+
+
+var fullname = "Emam hasan";
+document.write(fullname + "is my name");
+
+var num1 = 20;
+var num2 = 30;
+document.write( "num1 = " + num1  + "and num" + num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
